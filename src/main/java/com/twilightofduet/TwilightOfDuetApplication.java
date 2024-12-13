b5456c590befaa59f476b77c53a53930e0c7809b
tsutsumi miki <tsutsumi miki@192.168.1.11>
2024-12-13 22:57:48 +0900

    Start Project "TwilightOfDuet"

---
+++ src/main/java/com/twilightofduet/TwilightOfDuetApplication.java
@@ -0,0 +1,13 @@
+package com.twilightofduet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilightOfDuetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilightOfDuetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twilightofduet/TwilightOfDuetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twilightofduet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilightOfDuetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
