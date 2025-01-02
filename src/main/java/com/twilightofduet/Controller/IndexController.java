17e89054f9c1938faa72a1596b508c846c55af4a
tsutsumi miki <tsutsumi miki@192.168.1.11>
2025-01-02 15:23:29 +0900

    add main/resources layout html css

---
+++ src/main/java/com/twilightofduet/Controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.twilightofduet.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * IndexController
+ * 作成者 tsutsumi miki
+ * 編集日 2024/12/24 tsutsumi miki
+ */
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/top")
+	public String Top() {
+		return "top";
+	}
+
+}
