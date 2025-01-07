3856ad1997e31ca41271b16ba843196ef068268d
tsutsumi miki <tsutsumi miki@192.168.1.20>
2025-01-08 06:45:18 +0900

    キャラクターのカラー変更

--- src/main/java/com/twilightofduet/Controller/CharactorController.java
+++ src/main/java/com/twilightofduet/Controller/CharactorController.java
@@ -12,31 +12,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CharactorController {
 	
+	/**
+	 * 最初に表示されるキャラクター紹介画面
+	 * @return
+	 */
 	@GetMapping("/charactor")
 	public String charactorTopDisplay() {
 		return "charactor/narumi_chiharu";
 	}
 	
+	/**
+	 * 園山　巧美の紹介画面
+	 * @return
+	 */
 	@GetMapping("/charactor/SonoyamaTakumi")
 	public String charactorTakumiDisplay() {
 		return "charactor/sonoyama_takumi";
 	}
 
+	/**
+	 * 小早川　颯真の紹介画面
+	 * @return
+	 */
 	@GetMapping("/charactor/KobayakawaSoma")
 	public String charactorSomaDisplay() {
 		return "charactor/kobayakawa_soma";
 	}
 	
+	/**
+	 * 須王　御幸の紹介画面
+	 * @return
+	 */
 	@GetMapping("/charactor/SuoMiyuki")
 	public String charactorMiyukiDisplay() {
 		return "charactor/suo_miyuki";
 	}
 	
+	/**
+	 * 園山　巧斗の紹介画面
+	 * @return
+	 */
 	@GetMapping("/charactor/SonoyamaTakuto")
 	public String charactorTakutoDisplay() {
 		return "charactor/sonoyama_takuto";
 	}
 	
+	/**
+	 * 相良　実瑠の紹介画面
+	 * @return
+	 */
 	@GetMapping("/charactor/AiraMiru")
 	public String charactorMiruDisplay() {
 		return "charactor/aira_miru";
