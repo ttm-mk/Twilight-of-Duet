6ef86a79f173864802034a8159f6fcb896b90126
tsutsumi miki <tsutsumi miki@192.168.1.20>
2025-01-11 20:07:31 +0900

    ギャラリー要素新規追加

---
+++ src/main/java/com/twilightofduet/Controller/GalleryController.java
@@ -0,0 +1,24 @@
+package com.twilightofduet.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * IndexController
+ * 作成者 tsutsumi miki
+ * 編集日 2025/1/11 tsutsumi miki
+ */
+
+@Controller
+public class GalleryController {
+	
+	/**
+	 * ギャラリーのトップ画面/メインギャラリー
+	 */
+	@GetMapping("/gallery")
+	public String GalleryTopDisplay() {
+		return "gallery/gallery_all";
+		
+	}
+
+}
