26c227fd5e3ea7654c2d69b9ca8f76f82fca5971
tsutsumi miki <tsutsumi miki@192.168.1.20>
2025-01-19 16:51:52 +0900

    save and load feature 新規追加（html,controller,sidebar) header 修正

---
+++ src/main/java/com/twilightofduet/Controller/SaveLoadController.java
@@ -0,0 +1,26 @@
+package com.twilightofduet.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * IndexController
+ * 作成者 tsutsumi miki
+ * 編集日 2025/1/19 tsutsumi miki
+ */
+
+@Controller
+public class SaveLoadController {
+	
+	@GetMapping("/save")
+	public String saveDisplay() {
+		return "/saveLoad/save";
+	}
+	
+	@GetMapping("/load")
+	public String loadDisplay() {
+		return "/saveLoad/load";
+	}
+
+
+}
