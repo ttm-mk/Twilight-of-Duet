844e253ece878f924e5a247b6010d812c5028146
tsutsumi miki <tsutsumi miki@192.168.1.20>
2025-01-22 00:20:08 +0900

    InquiryForm　新規追加、inquiry feature　追加

--- src/main/java/com/twilightofduet/Controller/InquiryController.java
+++ src/main/java/com/twilightofduet/Controller/InquiryController.java
@@ -1,10 +1,16 @@
 package com.twilightofduet.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.twilightofduet.Form.InquiryForm;
 
 /*
- * IndexController
+ * InquiryController
  * 作成者 tsutsumi miki
  * 編集日 2025/1/19 tsutsumi miki
  */
@@ -12,9 +18,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InquiryController {
 	
+	/**
+	 * お問い合わせフォーム画面
+	 * @return
+	 */
 	@GetMapping("/inquiry")
 	public String inquiryDisplay() {
-		return "/inquiry";
+		return "/inquiry/inquiry_form";
+	}
+	
+	/**
+	 * 記載内容確認画面に遷移
+	 * 
+	 * @param inquiryForm　お問い合わせフォームクラス
+	 * @param model　モデルクラス
+	 * @return　内容確認画面
+	 */
+	@PostMapping("/inquiry/confirm")
+	public String inquiryConfirmDisplay(InquiryForm inquiryForm, Model model) {
+		/* ユーザー名出力 */
+		model.addAttribute("InquiryUserName", inquiryForm.getInquiryUserName());
+		/* お問い合わせ内容出力 */
+		model.addAttribute("InquiryText", inquiryForm.getInquiryText());
+		return "/inquiry/inquiry_confirm";
+
+	}
+	
+	@PostMapping("/inquiry/submit")
+	public String inquirySubmit(@ModelAttribute InquiryForm inquiryForm, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("message", "送信しました。ご意見ありがとうございました。");
+		return "redirect:/inquiry";
 	}
 
 }
---
+++ src/main/java/com/twilightofduet/Form/InquiryForm.java
@@ -0,0 +1,51 @@
+package com.twilightofduet.Form;
+
+/*
+ * InquiryForm
+ * 作成者 tsutsumi miki
+ * 編集日 2025/1/21 tsutsumi miki
+ */
+
+public class InquiryForm {
+	
+	/* お問い合わせユーザー名 */
+	private String InquiryUserName;
+	
+	/* お問い合わせ内容 */
+	private String InquiryText;
+
+	/**
+	 * お問い合わせユーザー名のGetter
+	 * @return
+	 */
+	public String getInquiryUserName() {
+		return InquiryUserName;
+	}
+
+	/**
+	 * お問い合わせユーザー名のSetter
+	 * @param inquiryUserName
+	 */
+	public void setInquiryUserName(String inquiryUserName) {
+		this.InquiryUserName = inquiryUserName;
+	}
+
+	/**
+	 * お問い合わせ内容のGetter
+	 * @return
+	 */
+	public String getInquiryText() {
+		return InquiryText;
+	}
+
+	/**
+	 * お問い合わせ内容のSetter
+	 * @param inquiryText
+	 */
+	public void setInquiryText(String inquiryText) {
+		this.InquiryText = inquiryText;
+	}
+	
+	
+
+}
