d58fe7856e4fb5a02ea68e60385450f2cbe9bd7f
tsutsumi miki <tsutsumi miki@192.168.1.56>
2025-04-20 01:45:57 +0900

    storymain記載・ファイル各種追加

--- src/main/java/com/twilightofduet/Story/StoryController.java
+++ src/main/java/com/twilightofduet/Story/StoryController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 /**
  *  StoryController
  * 作成者 tsutsumi miki
- * 編集日 2025/4/13 tsutsumi miki
+ * 編集日 2025/4/20 tsutsumi miki
  */
 
 @Controller
@@ -22,12 +22,21 @@ public class StoryController {
 	}
 	
 	/**
+	 * メインストーリー1話
+	 * @return
+	 */
+	@GetMapping("/story/main/1")
+	public String StoryMain1() {
+		return "story/charactor/main/story_main1.html";
+	}
+	
+	/**
 	 * キャラクターストーリー巧美1話
 	 * @return
 	 */
 	@GetMapping("/story/SonoyamaTakumi/1")
 	public String StoryTakumi1( ) {
-		return "story/charactor/sonoyama_takumi/story_pases/story_takumi1.html";
+		return "story/charactor/sonoyama_takumi/story_takumi1.html";
 	}
 
 }
---
+++ src/main/resources/static/js/story/common/story_main.js
@@ -0,0 +1,42 @@
+/**
+ * サイドバー：メイン制御
+ * 更新日：2025/4/20
+ */
+
+	console.log('JSファイルが読み込まれてます！');
+	
+	document.addEventListener('DOMContentLoaded', function () {
+	  console.log('DOM読み込み完了！');
+	});
+		
+	
+	function selectStoryMain() {
+	  const pathLinksMain = document.getElementById('pathLinks_main');
+	  const clickAreaMain = document.getElementById('clickArea_main');
+
+	    pathLinksMain.style.display = 'block';
+	    pathLinksMain.innerHTML = ''; // リセット
+	
+	    // リンク作成
+	    const link1 = document.createElement('a');
+	    link1.href = '/TwilightOfDuet/story/main/1'; // 遷移先のパス(Controllerで設定したパス)
+	    link1.textContent = '▶ 1話';
+	    link1.style.display = 'block';
+	    link1.style.margin = '5px 0';
+	
+//	    const link2 = document.createElement('a');
+//	    link2.href = '/TwilightOfDuet/story/main/2';
+//	    link2.textContent = '▶ 2話';
+//	    link2.style.display = 'block';
+//	    link2.style.margin = '5px 0';
+	
+	    pathLinksMain.appendChild(link1);
+//	    pathLinksTakumi.appendChild(link2);
+	
+	  }
+//	}
+	
+	document.addEventListener('DOMContentLoaded', function () {
+		document.getElementById('clickArea_main').addEventListener('click', selectStoryMain);
+	});
+	
