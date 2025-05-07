2ca0f3ae9c93d94e3bded346714db305894345a5
tsutsumi miki <tsutsumi miki@192.168.1.7>
2025-05-07 22:41:22 +0900

    charactor=>character修正

---
+++ src/main/java/com/twilightofduet/Screan/CharacterController.java
@@ -0,0 +1,69 @@
+package com.twilightofduet.Screan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+ * characterController
+ * 作成者 tsutsumi miki
+ * 編集日 2025/1/5 tsutsumi miki
+ */
+
+@Controller
+public class CharacterController {
+	
+	/**
+	 * 最初に表示されるキャラクター紹介画面
+	 * @return
+	 */
+	@GetMapping("/character")
+	public String characterTopDisplay() {
+		return "character/narumi_chiharu";
+	}
+	
+	/**
+	 * 園山　巧美の紹介画面
+	 * @return
+	 */
+	@GetMapping("/character/SonoyamaTakumi")
+	public String characterTakumiDisplay() {
+		return "character/sonoyama_takumi";
+	}
+
+	/**
+	 * 小早川　颯真の紹介画面
+	 * @return
+	 */
+	@GetMapping("/character/KobayakawaSoma")
+	public String characterSomaDisplay() {
+		return "character/kobayakawa_soma";
+	}
+	
+	/**
+	 * 須王　御幸の紹介画面
+	 * @return
+	 */
+	@GetMapping("/character/SuoMiyuki")
+	public String characterMiyukiDisplay() {
+		return "character/suo_miyuki";
+	}
+	
+	/**
+	 * 園山　巧斗の紹介画面
+	 * @return
+	 */
+	@GetMapping("/character/SonoyamaTakuto")
+	public String characterTakutoDisplay() {
+		return "character/sonoyama_takuto";
+	}
+	
+	/**
+	 * 相良　実瑠の紹介画面
+	 * @return
+	 */
+	@GetMapping("/character/AiraMiru")
+	public String characterMiruDisplay() {
+		return "character/aira_miru";
+	}
+	
+}
